006b17f4aec1e291a03415447c866cd21ff2ec37
Tyler Samuelson <tylersam@gmail.com>
2023-08-09 08:41:21 +0100

    Add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
+  plugins: [require('daisyui')],
   content: [
+    './app/components/**/*.html.erb',
+    './app/components/**/*.rb',
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+  ],
+  daisyui: {
+    themes: ['emerald'],
+  },
 }
